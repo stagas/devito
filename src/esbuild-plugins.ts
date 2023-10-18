f6215ecf2ec056ab531945a7b4bda89c1c7caf9b
stagas <gstagas@gmail.com>
2023-10-18 12:05:56 +0300

    save

--- src/esbuild-plugins.ts
+++ src/esbuild-plugins.ts
@@ -40,11 +40,11 @@ function logDeco(s: string, contents: string) {
       //   i += toAdd.length + 1
       // }
       // else {
-        i = contents.indexOf(')', i + 1)
-        i = contents.indexOf('{', i + 1)
+      i = contents.indexOf(')', i + 1)
+      i = contents.indexOf('{', i + 1)
       const toAdd = ';console.groupCollapsed(' + JSON.stringify(fnName) + ');'
-        contents = contents.slice(0, i + 1) + toAdd + contents.slice(i + 1)
-        i += toAdd.length
+      contents = contents.slice(0, i + 1) + toAdd + contents.slice(i + 1)
+      i += toAdd.length
       // }
     }
   } while (i >= 0)
