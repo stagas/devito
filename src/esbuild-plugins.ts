bcc4ce0bc1de659d2ee80a51381ca4f3d18b7b58
stagas <gstagas@gmail.com>
2023-10-22 18:43:00 +0300

    save

--- src/esbuild-plugins.ts
+++ src/esbuild-plugins.ts
@@ -23,7 +23,7 @@ export function logIt(kind: string, text: string) {
 
 function logDeco(s: string, contents: string) {
   let i = -1, p1, p2, fnName: string
-  const regexp = new RegExp(`^\\s+${s}`, 'g')
+  const regexp = new RegExp(`^\\s+${s}`, 'gm')
   console.log(regexp)
   do {
     regexp.lastIndex = i + 1
