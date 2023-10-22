523817fd4fce92f06fbfdc79c77419537951b72b
stagas <gstagas@gmail.com>
2023-10-22 18:52:44 +0300

    save

--- src/esbuild-plugins.ts
+++ src/esbuild-plugins.ts
@@ -24,14 +24,14 @@ export function logIt(kind: string, text: string) {
 function logDeco(s: string, contents: string) {
   let i = -1, p1, p2, fnName: string
   const length = s.length
+  const regexp = new RegExp(`^\\s+${s}`, 'gm')
   // console.log(regexp)
   do {
-    const regexp = new RegExp(`^\\s+${s}`, 'gm')
-    regexp.lastIndex = i
+    regexp.lastIndex = i + 1
     console.log(regexp.exec(contents))
     const res = regexp.exec(contents)
     i = res?.index ?? -1
-    if (i >= 0) i += res!.length - length + 2
+    if (i >= 0) i += res!.length - length + 1
     if (i >= 0 && contents.lastIndexOf('//', i) <= contents.lastIndexOf('\n', i)) {
       const indentSize = i - contents.lastIndexOf('\n', i)
       p1 = i
