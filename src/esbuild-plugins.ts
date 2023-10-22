2461372ce46aba40ce81b648a313a5c1f5e072db
stagas <gstagas@gmail.com>
2023-10-22 19:51:56 +0300

    save

--- src/esbuild-plugins.ts
+++ src/esbuild-plugins.ts
@@ -43,7 +43,7 @@ function logDeco(s: string, contents: string) {
       const toAdd = ';log(' + JSON.stringify(`${fnName}`) + ');'
       contents = contents.slice(0, i + 1) + toAdd + contents.slice(i + 1)
       i += toAdd.length + 1
-      if (cs === 'fx') {
+      if (cs === '@fx ') {
         const res2 = contents.indexOf('\n' + '}'.padStart(indentSize), i)
         if (res2 >= 0) {
           i = res2
