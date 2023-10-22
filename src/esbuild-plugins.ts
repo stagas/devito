8f5884d6a5696e2f74286fd6c4bcc520777b38ee
stagas <gstagas@gmail.com>
2023-10-22 19:04:06 +0300

    save

--- src/esbuild-plugins.ts
+++ src/esbuild-plugins.ts
@@ -38,8 +38,8 @@ function logDeco(s: string, contents: string) {
 
       fnName = contents.slice(p1 + 1, p2).trim()
 
-      i = contents.indexOf(')', i)
-      i = contents.indexOf('{', i)
+      i = contents.indexOf(')', i+1)
+      i = contents.indexOf('{', i+1)
       const toAdd = ';log(' + JSON.stringify(fnName) + ');'
       if (s === 'get ') console.log(fnName, s, contents.slice(i + 1))
       contents = contents.slice(0, i + 1) + toAdd + contents.slice(i + 1)
